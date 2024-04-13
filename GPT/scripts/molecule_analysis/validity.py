be7658f867d5a20df49f7e5a11d1507ca4c2bafc
angr12 <rla120@ic.ac.uk>
2024-04-13 16:12:12 +0100

    rdkit to filter out invalid molecules

---
+++ GPT/scripts/molecule_analysis/validity.py
@@ -0,0 +1,23 @@
+from rdkit import Chem
+import pandas as pd
+
+path = 'scripts/generated_molecules/top_k.csv'
+folder_path = 'scripts/generated_molecules/'
+molecules = pd.read_csv(path)
+valid_molecules = []
+invalid_molecules = []
+
+for c, mol in enumerate(molecules['Molecules']):
+    m = Chem.MolFromSmiles(mol)
+    
+    if m is None:
+        invalid_molecules.append(mol)
+        print(f'Invalid molecule at index {c}: {mol}')
+    else:
+        valid_molecules.append(mol)
+                
+print(f'Valid molecules: {len(valid_molecules)}')
+print(f'Invalid molecules: {len(invalid_molecules)}')
+
+# valid_molecules = pd.DataFrame(valid_molecules, columns=['Valid_Molecules'])
+# valid_molecules.to_csv(folder_path + 'valid_molecules.csv', index=False)
