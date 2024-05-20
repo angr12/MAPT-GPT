697b0b0d9ca4959224cc07a24f5a284253811da9
angr12 <rla120@ic.ac.uk>
2024-05-20 12:42:12 +0100

    changed RO5.py to take in df

--- GPT/scripts/molecule_analysis/RO5.py
+++ GPT/scripts/molecule_analysis/RO5.py
@@ -2,10 +2,9 @@ from rdkit import Chem
 from rdkit.Chem import Descriptors
 import pandas as pd
 
-def append_RO5(path):
-    """Function to append RO5 descriptors and number of violations to the csv file of SMILES strings"""
-    molecules = pd.read_csv(path)
-    print(f'Number of molecules loaded: {len(molecules)}')
+def append_RO5(input_df: pd.DataFrame):
+    """Function to append RO5 descriptors and number of violations to the df of SMILES strings"""
+    print(f'Number of molecules loaded: {len(input_df)}')
     
     # list to store values of RO5 descriptors
     MW_list = list()
@@ -15,7 +14,7 @@ def append_RO5(path):
     num_v_list = list()
     
     # check for RO5 violations in each molecule
-    for molecule in molecules["Valid_Molecules"]:
+    for molecule in input_df:
         m = Chem.MolFromSmiles(molecule)
         MW = Descriptors.MolWt(m)
         HBA = Descriptors.NOCount(m)
@@ -31,9 +30,19 @@ def append_RO5(path):
         num_v_list.append(sum(num_v))
         
     # append new rows to df
-    molecules = molecules.assign(MW = MW_list, HBA = HBA_list, HBD = HBD_list, LogP = LogP_list, Num_violations = num_v_list)
-    molecules.to_csv(path, index=False)
+    d = {'MW': MW_list, 'HBA': HBA_list, 'HBD': HBD_list, 'LogP': LogP_list, 'Num_violations': num_v_list}
+    RO5 = pd.DataFrame(d)
+    return RO5
     
 if __name__ == "__main__":
     path = 'scripts/generated_molecules/valid_molecules.csv'
-    append_RO5(path)
+    molecules_df = pd.read_csv(path)
+    RO5_df = append_RO5(molecules_df['Smiles'])
+    print(RO5_df.head())
+    
+    # add to the csv
+    molecules_df = pd.concat([molecules_df, RO5_df], axis=1)
+    print(molecules_df.head())
+    # print(molecules_df.shape)
+    
+    molecules_df.to_csv(path, index=False)
