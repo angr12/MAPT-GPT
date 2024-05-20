a4e61a8afd2913bc35d1f6ecb610278a765bccc2
angr12 <rla120@ic.ac.uk>
2024-05-20 12:25:39 +0100

    changed SA_score.py to take in df

--- GPT/scripts/molecule_analysis/SA_score.py
+++ GPT/scripts/molecule_analysis/SA_score.py
@@ -7,21 +7,28 @@ import sys
 sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))  # add the path to SA_Score
 import sascorer
 
-def append_SA(path):
-    """Function to append SA score to the csv file of SMILES strings"""
-    molecules = pd.read_csv(path)
+def append_SA(input_df: pd.DataFrame):
+    """Function to append SA score for a df of SMILES strings"""
     
-    print(f'Number of molecules loaded: {len(molecules)}')
+    print(f'Number of molecules loaded: {len(input_df)}')
     synthetic_accessibility = []
     
-    for mol in molecules['Valid_Molecules']:
+    for mol in input_df:
         m = Chem.MolFromSmiles(mol)
         sa = sascorer.calculateScore(m)
         synthetic_accessibility.append(sa)
         
-    molecules = molecules.assign(SA_Score = synthetic_accessibility)
-    molecules.to_csv(path, index=False)
+    synthetic_accessibility = pd.DataFrame(synthetic_accessibility, columns=['SA_Score'])
+    return synthetic_accessibility
     
 if __name__ == "__main__":
     path = 'scripts/generated_molecules/valid_molecules.csv'
-    append_SA(path)
+    molecules = pd.read_csv(path)
+    sa_df = append_SA(molecules['Smiles'])
+    print(sa_df.head())
+    print(f'Average SA score of dataset: {sa_df["SA_Score"].mean()}')
+    
+    # add to the csv
+    molecules.insert(len(molecules.columns), 'SA_Score', sa_df)
+    print(molecules.head())
+    molecules.to_csv(path, index=False)
